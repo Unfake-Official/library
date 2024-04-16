12442fb4d2acbc515365b11582464290fc8fceaf
Marcos Godinho Filho <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2024-04-16 10:25:03 -0300

    fix: spectrogram generation working

--- library/audio_manipulation/spectrograms/audio_to_spectrogram.py
+++ library/audio_manipulation/spectrograms/audio_to_spectrogram.py
@@ -13,10 +13,18 @@ def plot_spectrogram(mel_spect: np.ndarray, sr: float):
 
 
 def audio_to_spectrogram(input_audio_file: str, output_image_file: str):
+    hl = 1024 # number of samples per time-step in spectrogram
+    hi = 256 # Height of image
+    wi = 2048 # Width of image
+
     y, sr = librosa.load(input_audio_file, sr=None)
+    window = y[0:wi*hl]
+
     # generate the mel spectrogram
-    mel_spect = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=1024)
-    mel_spect = librosa.power_to_db(mel_spect, ref=np.max)
+    D = np.abs(librosa.stft(y))**2
+    S = librosa.feature.melspectrogram(S=D, sr=sr)
+    S = librosa.feature.melspectrogram(y=window, sr=sr, n_fft=hl*2, hop_length=hl, n_mels=hi, fmax=10000)
+    S = librosa.power_to_db(S, ref=np.max)
 
     # plot_spectrogram(mel_spect, sr)
-    plt.imsave(output_image_file, mel_spect, cmap='viridis')
+    plt.imsave(output_image_file, S, cmap='gray')
