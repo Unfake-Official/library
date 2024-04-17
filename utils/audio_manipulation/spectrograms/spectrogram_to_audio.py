b481a14c4243bd370dbf8924976beb53b82e28c5
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-04-17 10:01:24 -0300

    wip: try to convert spectrogram to audio

--- utils/audio_manipulation/spectrograms/spectrogram_to_audio.py
+++ utils/audio_manipulation/spectrograms/spectrogram_to_audio.py
@@ -1,17 +1,21 @@
 import librosa
+import librosa.core
+import librosa.core.spectrum
+import librosa
 import librosa.display
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
 import soundfile as sf
+import scipy.signal
+import scipy.io.wavfile
 
 
 # Carregar a imagem do espectrograma
 # spectrogram_image = plt.imread('/Users/u22156/Downloads/5.png')
 
-img = Image.open('/Users/u22156/Downloads/5.png')
-data = np.asarray(img)
+img = Image.open('1.png')
+i = np.asarray(img)
 
 
 # Converter a imagem para um array numpy
@@ -21,7 +25,13 @@ data = np.asarray(img)
 #spec_complex = np.abs(spectrogram_data) * np.exp(1j * np.angle(spectrogram_data))
 #audio_signal = librosa.istft(spec_complex)
 
-S = librosa.feature.inverse.mel_to_stft(data)
-y = librosa.griffinlim(S)
+#wav = librosa.feature.inverse.mel_to_stft(i)
+#print(wav)
 
-sf.write('/Users/u22156/Documents/Teste spectrogramas/reconstructed.wav', y, 48000)
+#sf.write('meajuda.wav', np.ravel(wav), samplerate=48000)
+
+audio = librosa.core.spectrum.griffinlim(i)
+#scipy.io.wavfile.write('porfavor.wav', )
+scipy.io.wavfile.write('test.wav', 48000, np.array(audio, dtype=np.int16))
+#librosa.output.write_wav('test2.wav', audio, 48000)
+sf.write('stereo_file.wav', audio, 48000)
