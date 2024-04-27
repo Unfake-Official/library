0e02050f1fd2fe3a33edba0bf5e7700ae536d8b5
Marcos Godinho Filho <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2024-04-27 12:26:08 -0300

    refactor: mel spec -> cqt spec

--- utils/audio_manipulation/generate_spectrograms.py
+++ utils/audio_manipulation/generate_spectrograms.py
@@ -3,6 +3,7 @@ import librosa
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
+import PIL.Image as img
 
 
 def plot_spectrogram(mel_spect: np.ndarray, sr: float):
@@ -14,27 +15,24 @@ def plot_spectrogram(mel_spect: np.ndarray, sr: float):
 
 
 def audio_to_spectrogram(input_audio_file: str, output_image_file: str, save: bool = True):
-    hl = 1024 # number of samples per time-step in spectrogram
-    hi = 256 # Height of image
-    wi = 2048 # Width of image
+    y, sr = librosa.load(input_audio_file)
 
-    y, sr = librosa.load(input_audio_file, sr=None)
-    window = y[0:wi*hl]
-
-    # generate the mel spectrogram
-    S = librosa.feature.melspectrogram(y=window, sr=sr, n_fft=hl*2, hop_length=hl, n_mels=hi, fmax=10000)
-    S = librosa.power_to_db(S, ref=np.max)
+    C = np.abs(librosa.cqt(y, sr=sr))
+    C = librosa.amplitude_to_db(C, ref=np.max)
 
     if save:
-        # plot_spectrogram(mel_spect, sr)
-        plt.imsave(output_image_file, S, cmap='gray')
+        plt.imsave(output_image_file, C, cmap='gray')
+        image = img.open(output_image_file)
+        new_image = image.resize((256, 256))
+        new_image.save(output_image_file)
 
-    return S, sr
+    return C, sr
 
 
-input_folder = r"C:\Users\mcsgo\OneDrive\Documentos\VozesFalsas"
+input_folder = r"C:\Users\mcsgo\OneDrive\Documentos\temp"
 output_folder = r"C:\Users\mcsgo\OneDrive\Documentos\Espectrogramas"
-is_fake = True # true if the folder contains fake audios, otherwise false
+is_fake = True  # true if the folder contains fake audios, otherwise false
+
 
 def generate_spectrograms(input_folder, output_folder, is_fake):
     for subdirectory_input, _, _, in os.walk(input_folder):
@@ -42,15 +40,19 @@ def generate_spectrograms(input_folder, output_folder, is_fake):
 
             print(f'Processing in: {subdirectory_input}')
 
-            speaker_name = subdirectory_input.split('\\' if '\\' in subdirectory_input else '/')[-1] + '_Spectrograms'
+            speaker_name = subdirectory_input.split(
+                '\\' if '\\' in subdirectory_input else '/')[-1] + '_Spectrograms'
             subdirectory_output = os.path.join(output_folder, speaker_name)
 
             os.makedirs(subdirectory_output, exist_ok=True)
 
             for ix, file_name in enumerate(os.listdir(os.path.join(subdirectory_input, ('wavs' if not is_fake else '')))):
-                input_audio_file = os.path.join(subdirectory_input, ('wavs' if not is_fake else ''), file_name)
-                output_image_file = os.path.join(subdirectory_output, f'{ix+1}.png')
+                input_audio_file = os.path.join(
+                    subdirectory_input, ('wavs' if not is_fake else ''), file_name)
+                output_image_file = os.path.join(
+                    subdirectory_output, f'{ix+1}.png')
 
                 audio_to_spectrogram(input_audio_file, output_image_file)
+
 
 generate_spectrograms(input_folder, output_folder, is_fake)
