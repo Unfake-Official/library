69807309463d535bbe8b6c487a16cd26dcbd9d7e
Marcos Godinho Filho <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2024-05-04 17:15:31 -0300

    refactor: using keras class for model

--- classifier/model.py
+++ classifier/model.py
@@ -1,25 +1,37 @@
 import tensorflow as tf
 
 # CNN model sketch:
-def generate_model():
-    model = tf.keras.Sequential([
+class Classifier(tf.keras.Model):
+    def __init__(self):
+        super(Classifier, self).__init__()
+
         # image in grayscale (1 color dimension) with 256x256 px resolution
-        tf.keras.layers.Rescaling(1./255, input_shape=(256,256,1)),
+        self.rescale = tf.keras.layers.Rescaling(1./255, input_shape=(256,256,1))
 
-        tf.keras.layers.Conv2D(16, (16,16), activation='relu'),
-        tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+        # convolution + feature extraction
+        self.conv1 = tf.keras.layers.Conv2D(16, (16,16), activation='relu')
+        self.max_pool1 = tf.keras.layers.MaxPool2D(pool_size=2, strides=2)
+        self.conv2 = tf.keras.layers.Conv2D(32, (16,16), activation='relu')
+        self.max_pool2 = tf.keras.layers.MaxPool2D(pool_size=2, strides=2)
+        self.conv3 = tf.keras.layers.Conv2D(64, (16,16), activation='relu')
+        self.max_pool3 = tf.keras.layers.MaxPool2D(pool_size=2, strides=2)
+        self.dropout = tf.keras.layers.Dropout(0.5)
 
-        tf.keras.layers.Conv2D(32, (16,16), activation='relu'),
-        tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+        # dense layer + output
+        self.flatten = tf.keras.layers.Flatten()
+        self.d1 = tf.keras.layers.Dense(1024, activation='relu')
+        self.d2 = tf.keras.layers.Dense(3, activation='softmax')
 
-        tf.keras.layers.Conv2D(64, (16,16), activation='relu'),
-        tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+    def call(self, x):
+        x = self.rescale(x)
+        x = self.conv1(x)
+        x = self.max_pool1(x)
+        x = self.conv2(x)
+        x = self.max_pool2(x)
+        x = self.conv3(x)
+        x = self.max_pool3(x)
+        x = self.dropout(x)
 
-        tf.keras.layers.Dropout(0.5),
-
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(1024, activation='relu'),
-        tf.keras.layers.Dense(3, activation='softmax') # three outputs: true, false or other
-    ])
-
-    return model
+        x = self.flatten(x)
+        x = self.d1(x)
+        x = self.d2(x)
